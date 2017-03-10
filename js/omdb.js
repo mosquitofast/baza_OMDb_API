c347f97444aa06d21fd46bc6133eb153f5c5fccd
Łukasz Tokarski <lukasz_tokarski@o2.pl>
2017-03-10 18:51:42 +0100

    0.2.2
    Program wyszukuje wskazany film i poprawnie go wyświetla

--- js/omdb.js
+++ js/omdb.js
@@ -1,23 +1,46 @@
 $(document).ready(function() {
 	$(init);
 	
+	$('#results').hide();
+	
 	function init() {
 		var mSearch = $('#movieSearch');
 		mSearch.click(f_mSearch);
 		
 		function f_mSearch() {
 			var mTitle = $('#movieTitle').val();
-			alert("Wpisałeś: " + mTitle);
+			
+			
 			
 			$.ajax({
-				url:"http://www.omdbapi.com/?t=" + mTitle,
+				url:"http://www.omdbapi.com/?t=" + mTitle + "&plot=full",
 				dataType:"json",				
 				success:renderTables
-			}); //end of ajax
+			});
 		}
 		
+		
+		
 		function renderTables(response) {
-			alert("Film: " + response.Runtime);
-		}
+						
+			
+			var title = response.Title;
+			var yearProduction = response.Year;
+			var duration = response.Runtime;
+			var describe = response.Plot;
+			var imgURL = response.Poster;
+				
+				
+			for(var i in response) {
+				var resp = response[i];
+				$(".link").html(title);
+				$(".yProduce").html(yearProduction);
+				$(".last").html(duration);
+				$(".post").html(describe);
+				$(".poster").attr("src",imgURL);
+			}	
+			$('#results').fadeIn(3000);
+		
+		}	
 	}
 });
