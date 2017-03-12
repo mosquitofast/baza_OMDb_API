2d3681ff0682e735ac20fb606c95b648b9b457ce
Łukasz Tokarski <lukasz_tokarski@o2.pl>
2017-03-12 08:07:33 +0100

    0.3.4
    Program skończony

--- js/omdb.js
+++ js/omdb.js
@@ -1,55 +1,79 @@
-$(document).ready(function() {
+$(document).ready(function() 
+{
 	$(init);
 	$(hideTags);
-	$(showMoreOptions);
 	
 		
-	function init() {
+	function init() 
+	{
 		var mSearch = $('#movieSearch');
 		mSearch.click(f_mSearch);
 		
-		function f_mSearch() {
+		function f_mSearch() 
+		{
 			var mTitle = $('#movieTitle').val();
 						
-			$.ajax({
+			$.ajax(
+			{
 				url:"http://www.omdbapi.com/?t=" + mTitle + "&plot=full",
 				dataType:"json",				
 				success:renderTables
-			});
+			}
+			);
 		}
 		
 		
 		
-		function renderTables(response) {
+		function renderTables(response) 
+		{
 			var titleTh = new Array("Tytuł","Rok produkcji","Czas trwania","Opis","Zdjęcie");
 			var responseTd = new Array(response.Title,response.Year,response.Runtime,response.Plot,response.Poster);
 			var check = response.Response;
-						
+			var test = responseTd[2];
+			alert ("zmienna ma wartość: " + test);
 			//generator of th
 			$('thead').find('tr').empty();
-			for(var i = 0; i < titleTh.length; i++) {
+			for(var i = 0; i < titleTh.length; i++) 
+			{
 				$('thead').find('tr').append("<th>" + titleTh[i] + "</th>");
 			}
 			
 			//if find no films
-			if(check == "False") {
+			if(check == "False") 
+			{
 				$('#noFilms').fadeIn(1500).delay(2000).fadeOut(1500);
-			} else {
+			} 
+			else 
+			{
 				//generator of td
 				$('tbody').find('tr').empty();
-				for(var j = 0; j < responseTd.length; j++) {
-					if(responseTd[j] == response.Poster ) {
-						$('tbody').find('tr').append("<td><img src='" + responseTd[j] + "' /></td>");
-					} else {
-						$('tbody').find('tr').append("<td>" + responseTd[j] + "</td>");
+				for(var j = 0; j < responseTd.length; j++) 
+				{
+					if(responseTd[j] == "N/A")
+					{
+						$('tbody').find('tr').append("<td>brak</td>");
+					}
+					else 
+					{
+						if(responseTd[j] != response.Poster)
+						{
+							$('tbody').find('tr').append("<td>" + responseTd[j] + "</td>");
+						}
+						else 
+						{
+							$('tbody').find('tr').append("<td><img src='" + responseTd[j] + "' /></td>");
+						}
 					}
 				}
 				$('#results').fadeIn(3000);
 			}
-		}	
-	}
+				
+		}
+	}	
 	
-	function hideTags() {
+	
+	function hideTags() 
+	{
 		$('#results').hide();
 		$('#noFilms').hide();
 	}
